bf490f4f68cd98b1aecfa0bf68303953eb56f43e
juliohtm08 <jhtomoyuki345@gmail.com>
2025-02-19 17:15:04 -0300

    Trabalhando com relacionamentos - parte 2

--- exercícios/exercicio02/src/models/Order.js
+++ exercícios/exercicio02/src/models/Order.js
@@ -10,6 +10,7 @@
 */
 
 const { query, getClient } = require('../database');
+const Customer = require('./Customer');
 const Product = require('./Product');
 
 class Order {
@@ -43,7 +44,16 @@ class Order {
         FROM orders 
         JOIN customers ON customers.id = orders.customer_id;
     `);
-    return result.rows.map((row) => new Order(row));
+    return result.rows.map((row) => {
+      const customer = new Customer({
+        id: row['customer.id'],
+        name: row['customer.name'],
+        email: row['customer.email'],
+        created_at: row['customer.created_at'],
+        updated_at: row['customer.updated_at'],
+      });
+      return new Order(row, customer);
+    });
   }
 
   /**
@@ -51,10 +61,12 @@ class Order {
    *   @param { number } customerId
    *   @param {{ id: number, quantity: number }[]} orderProducts
    */
+
+  // Cria um novo pedido
   static async create(customerId, orderProducts) {
     // Obtém os dados do produto a partir do seu id
     const storedOrderProducts = await query(
-      `SELECT * FROM products WHERE id = ANY($1::[]);`,
+      `SELECT * FROM products WHERE id = ANY($1::int[]);`,
       [orderProducts.map((product) => product.id)]
     );
 
@@ -69,13 +81,15 @@ class Order {
       return { product: new Product(row), quantity };
     });
 
+    // retorna uma conexão direta(client) da pool de conexões
     const dbClient = await getClient();
+
     let response;
     try {
       // inicia uma transação
       await dbClient.query('BEGIN');
 
-      // salva o pedido
+      // insere o pedido na tabela orders
       const orderCreationResult = await dbClient.query(
         `INSERT INTO orders (customer_id, total) VALUES ($1, $2) RETURNING *;`,
         [customerId, orderTotal]
@@ -88,7 +102,8 @@ class Order {
         populatedOrderProducts
       );
 
-      // para cada produto que foi pedido irá inserir um anova linha na tabela order_products
+      // para cada produto no pedido, insere uma nova linha na tabela order_products,
+      // que relaciona o pedido aos produtos
       for (const entry of populatedOrderProducts) {
         await dbClient.query(
           `INSERT INTO order_products (order_id, product_id, quantity) VALUES ($1, $2, $3);`,
@@ -96,7 +111,7 @@ class Order {
         );
       }
 
-      // commita a transação
+      // confirma a transação
       await dbClient.query('COMMIT');
       response = order;
     } catch (error) {
@@ -108,4 +123,64 @@ class Order {
 
     return response;
   }
+
+  static async findById(id) {
+    const orderResult = await query(
+      `SELECT
+            orders.*,
+            customers.id AS "customer.id",
+            customers.name AS "customer.name",
+            customers.email AS "customer.email",
+            customers.created_at AS "customer.created_at",
+            customers.updated_at AS "customer.updated_at"
+        FROM orders 
+        JOIN customers ON customers.id = orders.customer_id
+        WHERE orders.id = $1;`,
+      [id]
+    );
+    const orderProductResult = await query(
+      `SELECT order_products.*, products.*
+      FROM order_products
+      JOIN products ON order_products.product_id = products.id
+      WHERE order_products.order_id = $1;`,
+      [id]
+    );
+
+    const orderData = orderResult.rows[0];
+    const customer = new Customer({
+      id: orderData['customer.id'],
+      name: orderData['customer.name'],
+      email: orderData['customer.email'],
+      created_at: orderData['customer.created_at'],
+      updated_at: orderData['customer.updated_at'],
+    });
+
+    return new Order(orderData, customer, orderProductResult.rows);
+  }
+
+  static async delete(id) {
+    const dbClient = await getClient();
+
+    let result;
+    try {
+      await dbClient.query('BEGIN');
+      await dbClient.query(`DELETE FROM order_products WHERE order_id = $1`, [
+        id,
+      ]);
+      /* ao acionar este método, ele não irá funcionar de primeira, pois viola a restruição das FK, logo o mais adequado seria adicionar
+        o CASCADE as tabelas relacionadas */
+      await dbClient.query(`DELETE FROM orders WHERE id = $1;`, [id]);
+      await dbClient.query('COMMIT');
+
+      result = { message: 'Order deleted successfully' };
+    } catch (error) {
+      await dbClient.query('ROLLBACK');
+      result = { message: 'Error while deleting order!' };
+    } finally {
+      dbClient.release();
+    }
+    return result;
+  }
 }
+
+module.exports = Order;
--- exercícios/exercicio02/src/router.js
+++ exercícios/exercicio02/src/router.js
@@ -1,19 +1,28 @@
 const { Router } = require('express');
 const customerController = require('./controller/customers-controller');
 const productsController = require('./controller/products-controller');
+const ordersController = require('./controller/orders-controller');
 
 const router = Router();
 
+// rota de produtos
 router.get('/products', productsController.index);
 router.get('/products/:id', productsController.show);
 router.post('/products', productsController.create);
 router.put('/products/:id', productsController.update);
 router.delete('/products/:id', productsController.delete);
 
+// rota de clientes
 router.get('/customers', customerController.index);
 router.get('/customers/:id', customerController.show);
 router.post('/customers', customerController.create);
 router.put('/customers/:id', customerController.update);
 router.delete('/customers/:id', customerController.delete);
+
+// rota de pedidos
+router.get('/orders', ordersController.index);
+router.post('/orders', ordersController.create);
+router.get('/orders/:id', ordersController.show);
+router.delete('/orders/:id', ordersController.delete);
 
 module.exports = router;
