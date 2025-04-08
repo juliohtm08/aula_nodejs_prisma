93ef692bc1c775bd1c6cb624fcc0ff815cf989bd
juliohtm08 <jhtomoyuki345@gmail.com>
2025-04-08 15:55:10 -0300

    Criando um repositório para as leads - parte 01

---
+++ projeto-final/src/repositories/LeadsRepository.ts
@@ -0,0 +1,46 @@
+import { Lead } from '@prisma/client';
+
+export type LeadStatus =
+  | 'New'
+  | 'Contacted'
+  | 'Qualified'
+  | 'Converted'
+  | 'Unresponsive'
+  | 'Disqualified'
+  | 'Archived';
+
+export interface LeadWhereParams {
+  name?: {
+    like?: string;
+    equals?: string;
+    mode?: 'default' | 'insensitive';
+  };
+  status?: LeadStatus;
+}
+
+export interface FindLeadParams {
+  where?: LeadWhereParams;
+  sortBy?: 'name' | 'status' | 'createdAt';
+  order?: 'asc' | 'desc';
+  limit?: number;
+  offset?: number;
+}
+
+export interface CreateLeadAttributes {
+  name: string;
+  email: string;
+  phone: string;
+  status?: LeadStatus;
+}
+
+export interface LeadsRepository {
+  find: (param: FindLeadParams) => Promise<Lead[]>;
+  findById: (id: number) => Promise<Lead | null>;
+  count: (where: LeadWhereParams) => Promise<number>;
+  create: (attributes: CreateLeadAttributes) => Promise<Lead>;
+  updateById: (
+    id: number,
+    attributes: Partial<CreateLeadAttributes>
+  ) => Promise<Lead | null>;
+  DeleteById: (id: number) => Promise<Lead | null>;
+}
